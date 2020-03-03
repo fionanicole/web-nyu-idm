0edf7723473929b865b5f5fc29104ce7ab401917
fionanicole <fnh215@nyu.edu>
2020-03-03 16:20:32 -0500

    added porchpic

--- object-meditation/object-meditation-mythology/script.js
+++ object-meditation/object-meditation-mythology/script.js
@@ -1,2 +1,6 @@
 
-<input type="button" value="Go Back From Whence You Came!" onclick="history.back(-1)" />
+//SOURCE: https://stackoverflow.com/questions/20869401/display-an-image-only-when-button-is-clicked
+function porchdeck(){
+       var porchpic = "images/porchpic.jpg"
+       document.getElementById('porchpic').src = porchpic.replace("100px","100px");
+       }
