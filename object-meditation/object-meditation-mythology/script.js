5074d971a4a8102d965389c7681d58ef7e0e79a2
fionanicole <fnh215@nyu.edu>
2020-03-05 14:37:18 -0500

    fonts and oceans

--- object-meditation/object-meditation-mythology/script.js
+++ object-meditation/object-meditation-mythology/script.js
@@ -1,6 +1,6 @@
 
 //SOURCE: https://stackoverflow.com/questions/20869401/display-an-image-only-when-button-is-clicked
-function porchdeck(){
-       var porchpic = "images/porchpic.jpg"
-       document.getElementById('porchpic').src = porchpic.replace("100px","100px");
-       }
+// function porchdeck(){
+//        var porchpic = "images/porchpiccolor.png"
+//        document.getElementById('porchpic').src = porchpic.replace("width","height");
+//        }
