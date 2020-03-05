32349f3f816091b170eba52a77636998bb520a16
fionanicole <fnh215@nyu.edu>
2020-03-05 16:16:44 -0500

    frogboi added on index.html

--- object-meditation/object-meditation-mythology/script.js
+++ object-meditation/object-meditation-mythology/script.js
@@ -1,6 +1,7 @@
+$(document).ready(function() {
 
-//SOURCE: https://stackoverflow.com/questions/20869401/display-an-image-only-when-button-is-clicked
-// function porchdeck(){
-//        var porchpic = "images/porchpiccolor.png"
-//        document.getElementById('porchpic').src = porchpic.replace("width","height");
-//        }
+  $("#show").click(function() {
+    $(this).attr("src", "images/porchpiccolor.png");
+  });
+
+});
