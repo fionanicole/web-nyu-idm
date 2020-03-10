73ca00d54c7f30c7e10f6d224ee5c599ea01090d
fionanicole <fnh215@nyu.edu>
2020-03-10 00:09:19 -0400

    more frog fixes

--- object-meditation/object-meditation-mythology/script.js
+++ object-meditation/object-meditation-mythology/script.js
@@ -3,6 +3,6 @@ function myFunction() {
 }
 
 // https://stackoverflow.com/questions/14434604/i-want-to-delay-a-link-for-a-period-of-500-with-javascript
-function delay ("inspired-homepage.html") {
-    setTimeout( function() { window.location = "inspired-homepage.html"}, 10);
-}
+// function delay ("inspired-homepage.html") {
+//     setTimeout( function() { window.location = "inspired-homepage.html"}, 0.01);
+// }
