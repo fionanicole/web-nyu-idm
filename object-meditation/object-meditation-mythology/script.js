6517d98203a1cb6c64434eaff5462c51c49e262d
fionanicole <fnh215@nyu.edu>
2020-03-10 02:41:31 -0400

    more changes

--- object-meditation/object-meditation-mythology/script.js
+++ object-meditation/object-meditation-mythology/script.js
@@ -1,23 +1,23 @@
-// https://stackoverflow.com/questions/36607830/how-to-delay-a-hyperlink-until-a-css-animation-ends
-$(document).ready(function() {
-  $('.greycolored').click(function(myFunction) {
-    myFunction.preventDefault();
-    var $a = $(this).addClass('clicked');
-    setTimeout(function() {
-      window.location.assign($a.attr('href'));
-    }, 3000);
-  });
-});
-
+// // https://stackoverflow.com/questions/36607830/how-to-delay-a-hyperlink-until-a-css-animation-ends
+// $(document).ready(function() {
+//   $('.greycolored').click(function(myFunction) {
+//     myFunction.preventDefault();
+//     var $a = $(this).addClass('clicked');
+//     setTimeout(function() {
+//       window.location.assign($a.attr('href'));
+//     }, 3000);
+//   });
+// });
+//
 
 function colorize() {
   document.getElementById("greycolored").src = "images/porchpiccolor.png";
   setTimeout(delay(),5000);
 }
-
-function delay ("inspired-homepage.html") {
-    setTimeout( function() { window.location = "inspired-homepage.html"}, 5000);
-}
+//
+// function delay ("inspired-homepage.html") {
+//     setTimeout( function() { window.location = "inspired-homepage.html"}, 5000);
+// }
 
 function changeBackground() {
   document.getElementById("myVideo").src = "images/lavender.mp4";
