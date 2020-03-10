41327276581316224eb387677b40d1c6e9123345
fionanicole <fnh215@nyu.edu>
2020-03-10 02:13:33 -0400

    still trying to figure out how to get the link to delay

--- object-meditation/object-meditation-mythology/script.js
+++ object-meditation/object-meditation-mythology/script.js
@@ -1,8 +1,26 @@
-function myFunction() {
+// https://stackoverflow.com/questions/36607830/how-to-delay-a-hyperlink-until-a-css-animation-ends
+$(document).ready(function() {
+  $('.greycolored').click(function(myFunction) {
+    myFunction.preventDefault();
+    var $a = $(this).addClass('clicked');
+    setTimeout(function() {
+      window.location.assign($a.attr('href'));
+    }, 3000);
+  });
+});
+
+
+function colorize() {
   document.getElementById("greycolored").src = "images/porchpiccolor.png";
+  setTimeout(delay(),5000);
+}
+
+function delay ("inspired-homepage.html") {
+    setTimeout( function() { window.location = "inspired-homepage.html"}, 5000);
+}
+
+function changeBackground() {
+  document.getElementById("myVideo").src = "images/lavender.mp4";
 }
 
 // https://stackoverflow.com/questions/14434604/i-want-to-delay-a-link-for-a-period-of-500-with-javascript
-// function delay ("inspired-homepage.html") {
-//     setTimeout( function() { window.location = "inspired-homepage.html"}, 0.01);
-// }
