ff87d8053275b2a5e72c3a4745994873be285d06
fionanicole <fnh215@nyu.edu>
2020-03-20 17:07:38 -0400

    more commits

--- object-meditation/script.js
+++ object-meditation/script.js
@@ -1,26 +1,18 @@
-// // https://stackoverflow.com/questions/36607830/how-to-delay-a-hyperlink-until-a-css-animation-ends
-// $(document).ready(function() {
-//   $('.greycolored').click(function(myFunction) {
-//     myFunction.preventDefault();
-//     var $a = $(this).addClass('clicked');
-//     setTimeout(function() {
-//       window.location.assign($a.attr('href'));
-//     }, 3000);
-//   });
-// });
-//
-
-// function colorize() {
-//   document.getElementById("greycolored").src = "images/porchpiccolor.png";
-//   setTimeout(delay(), 5000);
-// }
-//
-// function delay ("inspired-homepage.html") {
-//     setTimeout( function() { window.location = "inspired-homepage.html"}, 5000);
-// }
+function colorize() {
+  document.getElementById("greycolored").src = "images/porchpiccolor.png";
+}
 
 function changeBackground() {
   document.getElementById("myVideo").src = "images/lavender.mp4";
 }
 
-// https://stackoverflow.com/questions/14434604/i-want-to-delay-a-link-for-a-period-of-500-with-javascript
+// SOURCE: https://stackoverflow.com/questions/9877263/time-delayed-redirect
+
+$('#shiftToPage').click (function(event) {
+   event.preventDefault(); //will stop the link href to call the blog page
+
+   setTimeout(function () {
+       window.location.href = "inspired-homepage.html"; //will redirect to your blog page (an ex: blog.html)
+    }, 10000); //will call the function after 2 secs.
+
+});
